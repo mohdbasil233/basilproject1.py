2c5148fc6909f90bc0cfe83ea7a09ffe383baa7b
mohdbasil233 <mohdbasil2007@gmail.com>
2026-07-09 12:27:49 +0500

    Add files via upload

---
+++ project.py
@@ -0,0 +1,25 @@
+st=input("enter your message:")
+words=st.split(" ")
+coding=input("1 for decoding and 0 for encoding:")
+coding=True if(coding=="1") else False
+nwords=[]
+if coding:
+    for word in words:
+        if len(word)>=3:
+          r1="dfg"
+          r2="ede"
+          stnew=r1+word[1:]+word[0]+r2
+          nwords.append(stnew)
+        else:
+         nwords.append(word[::-1])
+
+else:
+     for word in words:
+        if len(word)>=3:
+            stnew = word[3:-3]         
+            stnew = stnew[-1] + stnew[:-1]  
+            nwords.append(stnew)
+        else:
+            nwords.append(word[::-1])
+
+print(" ".join(nwords))
